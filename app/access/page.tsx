2b16daaa1f63c9fb1f69f8ac9e8bddb6e17f0c5b
Izumi Rinna <izumi@genech.local>
2025-06-24 15:38:57 +0900

    Initial commit

--- app/access/page.tsx
+++ app/access/page.tsx
@@ -3,7 +3,7 @@ export default function AccessPage() {
   return (
     <main>
       <h1>アクセス情報</h1>
-      <p>ここええええ?????住所や地図を載せてもいいね！！！！！</p>
+      <p>もうそろいいよね住所や地図を載せてもいいね！！！！！</p>
     </main>
   );
 }
