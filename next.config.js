90a10a06d868b6971b0bce33a7cf5fbba762de6d
Izumi Rinna <izumi@genech.local>
2025-06-24 12:24:48 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  output: 'export', // ← 静的エクスポートモード
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // これで静的エクスポート指定
+  // 他の設定があればここに書く
+}
+
+module.exports = nextConfig
